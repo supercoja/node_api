1b7f678794662e8f92e70528cf8fc6e715499bd6
System Administrator <root@MacBook-Pro-de-Thiago-Almeida.local>
2017-04-30 11:05:07 -0300

    cat.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var http = require("express");
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
 
-http.createServer(function(req,res) {
- res.writeHead(200,{
-     'Content-Type': 'text/plain'
- });
- res.end('Hello World\n');
-}).listen(3000,'127.0.0.1');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
+var cats = require('./cats.js')(app);
+
+
+var server = app.listen(3000,function(){
 console.log('Server running at http://127.0.0.1:3000/');
+});
+
