6567efb62060b1a09587aedd04242e98d3ffe908
Thiago André De Almeida <milladm03@millgest.com.br>
2017-05-03 16:29:52 -0300

    MongoDB Integration OK

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var app = express();
+
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-var cats = require('./cats.js')(app);
+var cats = require('./cat_routes.js')(app);
 
 
 var server = app.listen(3000,function(){
