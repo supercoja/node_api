a1a2d1bd3a82e121e5e3015cfce856c998aea90d
Thiago_Almeida <thiagoalmeida@MacBook-Pro-de-Thiago-Almeida.local>
2017-05-08 12:56:49 -0300

    Servers Ok

--- cat_server.js
+++ cat_server.js
@@ -1,3 +1,22 @@
+var express = require("express");
+var app = express();
+
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cats');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var cats = require('./routes/cat.js')(app);
+
+var server = app.listen(3002,function(){
+console.log('Server running at http://127.0.0.1:3000/');
+});
+
+/*
 var r = require('request').defaults({
    json: true
 })
@@ -7,6 +26,8 @@ module.exports = function(app){
 /*
  Read 
 */
+
+/*
  app.get('/pets',function(req, res){
      r({uri:'http://localhost:3001/dog'}, function(error, response, body) {
          if (!error && response.statusCode==200){
@@ -19,3 +40,4 @@ module.exports = function(app){
 };
 
 
+*/
--- routes/cat.js
+++ routes/cat.js
@@ -1,5 +1,5 @@
  var _ = require('lodash');
- var Cat = require('./cat_model.js');
+ var Cat = require('./cat.js');
 
  module.exports = function(app) {
   _cats = [];
